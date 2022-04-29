7f3c472872ed4dcbdb7cc2d3b9af1c57a396641b
dwm <dwmcrobb@me.com>
2022-04-29 18:41:43 -0400

    Added Doxygen main page.  Added mkarchive.sh.  Modified getvers.sh to use new scheme, which allows use of source tarball to build a package.

---
+++ classes/include/DwmCredenceDocMainPage.hh
@@ -0,0 +1,44 @@
+/*! @file DwmCredenceDocMainPage.hh
+ *  @brief Documentation main page (no source code)
+ * 
+ * \mainpage DwmCredence Class Library
+ *
+ *  \section intro_sec Introduction
+ *
+ *  This class library may be used for secure, authenticated network
+ *  communicator over TCP.  The main three classes are Peer, KeyStash
+ *  and KnownKeys.  Utilizing just these three classes, it is relatively
+ *  easy to create secure TCP applications.
+ *
+ *  The library depends on libsodium, boost::asio and libDwm.
+ *
+ *  Since I'm using libsodium, I'm using XChaCha20Poly1305 for
+ *  encryption.  User and server authentication uses signatures
+ *  produced and validated with Ed25519 keys.
+ *
+ *  No passwords are used, only key files (which aren't password
+ *  protected).  This is intentional since my main usage is in applications
+ *  that are not launched interactively.  I may later add password
+ *  protected keys.
+ *  
+ *  \section history_sec History
+ *
+ *  This library came about when I grew tired of problems introduced
+ *  in Crypto++ that forced me to maintain my own personal fork that
+ *  was needed by libDwmAuth.  In my own application, libDwmAuth will
+ *  be replaced by libDwmCredence.  The new name is a hint that it's
+ *  not the same as libDwmAuth under the hood, and allows me to migrate
+ *  my applications on an app-by-app basis.
+ *
+ *  \section platforms_sec Platforms
+ *
+ *  I really only maintain support for 4 platforms: FreeBSD, macOS,
+ *  desktop linux and Raspbian (now Raspberry Pi OS).
+ *  FreeBSD is my operating system of choice for servers and macOS is my
+ *  operating system of choice for desktops and laptops.  I have
+ *  several Raspberry Pis I utilize for simple small tasks, and Ubuntu
+ *  VMs and Ubuntu workstations.
+ *
+ *  \tableofcontents
+ *
+*/
