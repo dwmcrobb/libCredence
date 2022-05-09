2e20dd7705dc91eaf320d9df91a34cd1f9800094
dwm <Daniel.McRobb@leoni.com>
2022-05-09 19:14:03 -0400

    Push before tag.

--- mkrelease.sh
+++ mkrelease.sh
@@ -19,12 +19,14 @@ EchoOrExec () {
 	echo -e '\techo '${TAGNAME} '> latest_release'
 	echo -e '\t'git add latest_release
 	echo -e '\t'git commit -m \"Tagging as ${TAGNAME}\"
+	echo -e '\t'git push
 	echo -e '\t'git tag -a "${TAGNAME}" -m \"Tagged as ${TAGNAME}\"
-	echo -e '\t'${pfx}git push --tags
+	echo -e '\t'git push --tags
     else
 	echo ${TAGNAME} > latest_release
 	git add latest_release
 	git commit -m "Tagging as ${TAGNAME}"
+	git push
 	git tag -a "${TAGNAME}" -m "Tagged as ${TAGNAME}"
 	git push --tags
     fi
